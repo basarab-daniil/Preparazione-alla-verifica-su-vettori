79d9713c812c8ff43303740029b521aab48b1654
basarab-daniil <basarab.daniil@iisgalvanimi.edu.it>
2025-10-20 11:51:46 +0000

    Il componente container stampa il componente macchina per ogni macchina da cantiere contenuta nel vettore creato al punto2

--- preparazione/src/app/container/container.ts
+++ preparazione/src/app/container/container.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { Macchina } from '../macchina/macchina';
 
 @Component({
   selector: 'app-container',
-  imports: [],
+  imports: [Macchina],
   templateUrl: './container.html',
   styleUrl: './container.css'
 })
 export class Container {
-
+  macchine: string[];
+  constructor() {
+    this.macchine = ['Escavatore', 'Pala Meccanica', 'Rullo Compressore', 'Betoniera', 'Trivella', 'Carroponte', 'Grù', 'Carrello Elevatore', 'Trattore Cingolato', 'Livellatrice'];
+  }
+  ngOnInit () {
+  }
 }
---
+++ preparazione/src/app/macchina/macchina.ts
@@ -0,0 +1,14 @@
+import { Component, Input} from '@angular/core';
+
+@Component({
+  selector: 'app-macchina',
+  imports: [],
+  templateUrl: './macchina.html',
+  styleUrl: './macchina.css'
+})
+export class Macchina{
+  @Input() macchina!: string;
+  constructor() {
+  }
+  ngOnInit() {}
+}
